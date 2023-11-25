43fa46dd934280bdcc68ff8031c516427acb8cc0
thisSasha <trhfjfn@gmail.com>
2023-11-25 16:08:17 +0300

    Packet 1.2

--- packet.js
+++ packet.js
@@ -1,5 +1,5 @@
 import {lc} from './lc/listConstructor.js'
 import {vc} from './vc/valueConstructor.js'
-import {MMath, CColor, HHelp} from './ssimple/ssimple.js'
+import './ssimple/ssimple.js'
 
 export default {lc, vc, MMath, CColor, HHelp}
